7915d2c3a3ea19ad5ec735d3c3c2ff17d812ef7e
Suliman <salmanyousef13@gmail.com>
2026-01-07 17:20:54 +0300

    First app

---
+++ src/main/java/com/example/firstapp/FirstAppApplication.java
@@ -0,0 +1,23 @@
+package com.example.firstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class FirstAppApplication {
+
+    public static void main(String[] args) {
+
+        var ctx = SpringApplication.run(FirstAppApplication.class, args);
+
+        MyFirstClass myFirstClass = ctx.getBean(MyFirstClass.class);
+        System.out.println(myFirstClass.sayHello());
+    }
+
+    @Bean
+    public MyFirstClass myFirstClass() {
+        return new MyFirstClass();
+    }
+
+}
---
+++ src/main/java/com/example/firstapp/MyFirstClass.java
@@ -0,0 +1,11 @@
+package com.example.firstapp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyFirstClass {
+
+    public String sayHello() {
+        return "Hello, from MyFirstClass";
+    }
+}
